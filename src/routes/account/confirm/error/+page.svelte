b38587351f88b2591dfa32e5a05827f4f2201cce
hans <hkmelaa@hotmail.com>
2023-10-04 22:28:35 +0200

    haha funny message

--- src/routes/account/confirm/error/+page.svelte
+++ src/routes/account/confirm/error/+page.svelte
@@ -1,1 +1,3 @@
-<h1>Something went wrong verifying otp.</h1>
+<h1>Something went wrong verifying session!</h1>
+<h2>A team has not been dispatched to your location.</h2>
+<p>Please make sure to notify the person to your left that this solution works great</p>
