932abddf9843f3a4a13ffcbe02ccb7fbfd6ddfc2
hans <hkmelaa@hotmail.com>
2023-10-04 21:35:13 +0200

    fix: remove "next" redir, use index

--- src/routes/account/confirm/+server.js
+++ src/routes/account/confirm/+server.js
@@ -7,12 +7,11 @@ export const GET = async (event) => {
 	} = event;
 	const token_hash = url.searchParams.get('token_hash');
 	const type = url.searchParams.get('type');
-	const next = url.searchParams.get('next') ?? '/';
 
 	if (token_hash && type) {
 		const { error } = await supabase.auth.verifyOtp({ token_hash, type });
 		if (!error) {
-			throw redirect(303, `/${next.slice(1)}`);
+			throw redirect(303, '/');
 		}
 	}
 
