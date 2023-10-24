0c31b6557fdcef113fef3be9ee0bdd8cee2060ef
hans <hkmelaa@hotmail.com>
2023-10-24 23:46:41 +0200

    asd

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -3,18 +3,16 @@
 	<h2>Privacy Statement</h2>
 
 	<p>
-		We only store the data provided by you, like identity provider (github, linkedin, etc.), and
-		also what events you might be registered to.
+		We only store the data needed, like identity provider (github, linkedin, etc.), and also what
+		events you might be registered to.
 	</p>
 
 	<p>
 		If you wish to no longer have any specific data stored, unregister from any events and/or
 		disconnect your identity provider.
 	</p>
-</article>
-<article>
-	<h2>Contact</h2>
-	<p>For any questions regarding [stuff], or data...</p>
+
+	<p>For any questions or conserns, feel free to send us an email at [...]</p>
 </article>
 
 <style>
