7355304bf716d621df3e32526e34224a32be9c83
hans <hkmelaa@hotmail.com>
2023-10-25 00:20:28 +0200

    errortainmentsystem

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,30 @@
 <script>
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import OneArmed from '$lib/OneArmed.svelte';
 </script>
 
-<svelte:head>
-	<title>Error {$page.status}</title>
-</svelte:head>
-
 <h1>😬 {$page.status}: {$page.error.message}</h1>
+
+<section>
+	<h2>Errortainment</h2>
+	<OneArmed
+		coin={100}
+		on:gameover={() => {
+			goto('/');
+		}}
+	/>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	h2 {
+		margin-bottom: 2rem;
+		color: var(--color-d);
+	}
+</style>
